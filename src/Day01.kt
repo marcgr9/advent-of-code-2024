13a69c99afe10c437682d786c37e773a237aaed4
Marc Gruita <marc@gruita.ro>
2024-12-01 22:23:43 +0200

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,39 @@
+import kotlin.math.abs
+
 fun main() {
+    fun extractLists(input: List<String>): Pair<List<Int>, List<Int>> {
+        return input.fold(initial = Pair(mutableListOf<Int>(), mutableListOf<Int>())) { acc, it ->
+            it.split("   ").let {
+                acc.first.add(it[0].toInt())
+                acc.second.add(it[1].toInt())
+            }
+            acc
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (left, right) = extractLists(input)
+
+        return left.sorted().zip(right.sorted()).fold(initial = 0) { acc, it ->
+            acc + abs(it.second - it.first)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (left, right) = extractLists(input)
+
+        val frequencies = Array(100_000) { 0 }
+        right.forEach {
+            frequencies[it]++
+        }
+
+        return left.fold(initial = 0) { acc, it -> acc + frequencies[it] * it }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
